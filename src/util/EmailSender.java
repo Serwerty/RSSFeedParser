e4ec6c4b23e9719354f7a8224b5b92b07740c28a
kateliman <kate.liman@itechcraft.com>
2017-06-25 23:04:11 +0300

    added attachments to mail

--- src/util/EmailSender.java
+++ src/util/EmailSender.java
@@ -6,14 +6,15 @@ import java.util.Properties;
 import constants.EmailConstants;
 import constants.EmailType;
 
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
 /**
  * Created by liman on 24.06.2017.
  */
@@ -41,40 +42,71 @@ public class EmailSender {
         return instance;
     }
 
-        public void SendEmail(EmailType emailType, String text){
-            props.put(EmailConstants.KEY_HOST, EmailConstants.VAL_HOST);
-            props.put(EmailConstants.KEY_PORT, EmailConstants.VAL_PORT);
-            props.put(EmailConstants.KEY_SSL, true);
-            props.put(EmailConstants.KEY_AUTH, true);
+    public void SendEmail (EmailType emailType, String text, String[] attachments)
+    {
+        props.put(EmailConstants.KEY_HOST, EmailConstants.VAL_HOST);
+        props.put(EmailConstants.KEY_PORT, EmailConstants.VAL_PORT);
+        props.put(EmailConstants.KEY_SSL, true);
+        props.put(EmailConstants.KEY_AUTH, true);
 
-            Session session = Session.getInstance(props, authenticator);
-            //session.setDebug(debug); // to log??
+        Session session = Session.getInstance(props, authenticator);
+        //session.setDebug(debug);
 
-            Message message = new MimeMessage(session);
-            try {
-                InternetAddress[] address = {new InternetAddress(EmailConstants.EMAIL_DEFAULT_RECIPIENT)};
-                message.setRecipients(Message.RecipientType.TO, address);
-                switch (emailType){
-                    case Error:
-                        message.setSubject(EmailConstants.ERROR_SUBJECT);
-                        break;
-                    case Statistics:
-                        message.setSubject(EmailConstants.STATISTICS_SUBJECT);
-                        break;
-                    default:
-                        message.setSubject(EmailConstants.DEFAULT_SUBJECT);
-                        break;
-                }
-                message.setSentDate(new Date());
-                message.setText(text);
-                Transport.send(message);
-                Logger.get().addMessage(String.format("EMAIL INFO: Email of type \"%s\"" +
-                        " was sent to %s.", emailType.toString(), address[0].toString()));
-            } catch (MessagingException e) {
-                Logger.get().addMessage("EMAIL ERROR: Error while sending email.");
-                throw new RuntimeException(e);
+        Message message = new MimeMessage(session);
+        try {
+            InternetAddress[] address = {new InternetAddress(EmailConstants.EMAIL_DEFAULT_RECIPIENT)};
+            message.setRecipients(Message.RecipientType.TO, address);
+            switch (emailType){
+                case Error:
+                    message.setSubject(EmailConstants.ERROR_SUBJECT);
+                    break;
+                case Statistics:
+                    message.setSubject(EmailConstants.STATISTICS_SUBJECT);
+                    break;
+                default:
+                    message.setSubject(EmailConstants.DEFAULT_SUBJECT);
+                    break;
             }
+            message.setSentDate(new Date());
+            if (attachments.length> 0) {
+                // https://www.tutorialspoint.com/javamail_api/javamail_api_send_email_with_attachment.htm
+                BodyPart messageBodyPart = new MimeBodyPart();
+                messageBodyPart.setText(text);
+                Multipart multipart = new MimeMultipart();
+                multipart.addBodyPart(messageBodyPart);
+
+                for(String filePath:attachments)
+                {
+                    messageBodyPart = new MimeBodyPart();
+                    String filename = filePath;
+                    DataSource source = new FileDataSource(filename);
+                    messageBodyPart.setDataHandler(new DataHandler(source));
+                    messageBodyPart.setFileName(filename);
+                    multipart.addBodyPart(messageBodyPart);
+                }
+                message.setContent(multipart);
+            }
+            else{
+                message.setText(text);
+            }
+
+            Transport.send(message);
+            Logger.get().addMessage(String.format("EMAIL INFO: Email of type \"%s\"" +
+                    " was sent to %s.", emailType.toString(), address[0].toString()));
+        } catch (MessagingException e) {
+            Logger.get().addMessage("EMAIL ERROR: Error while sending email.");
+            throw new RuntimeException(e);
         }
+    }
 
+    public void SendEmail(EmailType emailType, String text)
+    {
+        String[] attachments = {};
+        SendEmail(emailType, text, attachments);
+    }
 
+    public void SendEmail(EmailType emailType, String text, String pathToFile){
+        String[] attachments = {pathToFile};
+        SendEmail(emailType, text, attachments);
+    }
 }
